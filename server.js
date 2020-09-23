3a2aa4782997dc0ace748d9a5e3ed097762f48ad
ewa81 <ewa.strzelecka@icloud.com>
2020-09-23 19:37:37 +0200

    Done second.

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 const todoRouter = require('./routes/todo');
 app.use('/api', todoRouter);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
