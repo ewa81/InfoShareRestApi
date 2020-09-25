36987a2ffe7ff6649e0a97de15fa2099054cfd63
ewa81 <ewa.strzelecka@icloud.com>
2020-09-25 23:33:33 +0200

    add new folder mocks and function insertMany().

--- routes/todo.js
+++ routes/todo.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const Todo = require('../models/todo.model');
+const todos = require('../mocks/todos.json');
 
-router.post('/todos', async(req, res) => {
+router.post('/todos/all', async(req, res) => {
   const newTodo = new Todo(req.body);
-  
+
   try {
     await newTodo.save();
     res.json(newTodo);
   } catch (error) {
     console.log(error);
   }
+  const todo = await Todo.insertMany(todos);
+  console.log(await Todo.find());
 });
 
 module.exports = router;
